4d94e1be55ab32942bd2f9f8cb50e81360f9ee8d
Sergey Vasilenko <i.am@vasilenkos.ru>
2013-09-10 02:51:23 +0500

    Unit test for XmlSerializersRepository

---
+++ Tests/LibraryExtensions/Helpers/XmlSerializersRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using NUnit.Framework;
+using DSE.Tests.Symptomatic;
+using System.Xml.Serialization;
+using System.Linq;
+
+namespace DSE.Tests.LibraryExtensions.Helpers
+{
+    public class XmlSerializersRepository : Symptomatic.SimpleTestCase
+    {
+        public class _TestClass
+        {
+        }
+
+        [Test]
+        public void DoCheck()
+        {
+            Assert.AreSame(
+                DSE.Extensions.XmlSerializersRepository.Acquire(typeof(_TestClass)),
+                DSE.Extensions.XmlSerializersRepository.Acquire(typeof(_TestClass))
+            );
+        }
+    }
+}
