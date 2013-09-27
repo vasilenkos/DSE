203af29faf10eba8346796c1b4934481457b6882
Sergey Vasilenko <i.am@vasilenkos.ru>
2013-09-27 16:22:12 +0500

    Mock for an 'object' tests added

---
+++ Tests/LibraryExtensions/Object.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+using DSE.Tests.Symptomatic;
+
+using DSE.Extensions;
+
+namespace DSE.Tests.LibraryExtensions
+{
+    public class _Object : Symptomatic.SimpleTestCase
+    {
+
+    }
+}
