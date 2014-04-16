d697e025661f2243d7675a88362e93dfb853b442
Sergey V. Vasilenko <sergey.sega.vasilenko@gmail.com>
2014-04-17 00:52:37 +0600

    Mono conditional test override

--- LibraryExtensions/Sorter.cs
+++ LibraryExtensions/Sorter.cs
@@ -8,101 +8,98 @@ using System.Text;
 
 namespace DSE.Extensions
 {
-    public enum SorterDirection
-    {
-        Ascending = 0,
-        Descending = 1
-    }
+	public enum SorterDirection
+	{
+		Ascending = 0,
+		Descending = 1
+	}
 
-    public class Sorter<T> : IComparer<T>
-    {
-        protected List<Func<T, T, Int32>> _oComparers = null;
+	public class Sorter<T> : IComparer<T>
+	{
+		protected List<Func<T, T, Int32>> _oComparers = null;
 
-        public Sorter()
-        {
-            _oComparers = new List<Func<T, T, int>>();
-        }
+		public Sorter ()
+		{
+			_oComparers = new List<Func<T, T, int>> ();
+		}
 
-        private int _CompareTwoBooleans(Boolean pbFirst, Boolean pbSecond)
-        {
-            return (pbFirst == pbSecond) ? 0 : (pbFirst ? 1 : -1);
-        }
+		private int _CompareTwoBooleans (Boolean pbFirst, Boolean pbSecond)
+		{
+			return (pbFirst && pbSecond) ? 0 : (pbFirst ? 1 : -1);
+		}
 
-        private Int32 _InvertResult(Int32 pnResult)
-        {
-            return pnResult * -1;
-        }
+		private Int32 _InvertResult (Int32 pnResult)
+		{
+			return pnResult * -1;
+		}
 
-        public Sorter<T> ByExpression(Func<T, Boolean> poFunctor)
-        {
-            return ByExpression(poFunctor, SorterDirection.Ascending);
-        }
+		public Sorter<T> ByExpression (Func<T, Boolean> poFunctor)
+		{
+			return ByExpression (poFunctor, SorterDirection.Ascending);
+		}
 
-        public Sorter<T> ByExpression(Func<T, Boolean> poFunctor, SorterDirection peDirection)
-        {
-            switch (peDirection)
-            {
-                case SorterDirection.Ascending:
-                    _oComparers.Add((x, y) => _CompareTwoBooleans(poFunctor(x), poFunctor(y)));
-                    break;
-                case SorterDirection.Descending:
-                    _oComparers.Add((x, y) => _InvertResult(_CompareTwoBooleans(poFunctor(x), poFunctor(y))));
-                    break;
-            }
+		public Sorter<T> ByExpression (Func<T, Boolean> poFunctor, SorterDirection peDirection)
+		{
+			switch (peDirection)
+			{
+			case SorterDirection.Ascending:
+				_oComparers.Add ((x, y) => _CompareTwoBooleans (poFunctor (x), poFunctor (y)));
+				break;
+			case SorterDirection.Descending:
+				_oComparers.Add ((x, y) => _InvertResult (_CompareTwoBooleans (poFunctor (x), poFunctor (y))));
+				break;
+			}
 
-            return this;
-        }
+			return this;
+		}
 
-        public Sorter<T> ByComparison<P>(Func<T, P> poFunctor)
-        {
-            return ByComparison(poFunctor, SorterDirection.Ascending);
-        }
+		public Sorter<T> ByComparison<P> (Func<T, P> poFunctor)
+		{
+			return ByComparison (poFunctor, SorterDirection.Ascending);
+		}
 
-        public Sorter<T> ByComparison<P>(Func<T, P> poFunctor, SorterDirection peDirection)
-        {
-            switch (peDirection)
-            {
-                case SorterDirection.Ascending:
-                    _oComparers.Add((x, y) => Comparer<P>.Default.Compare(poFunctor(x), poFunctor(y)));
-                    break;
-                case SorterDirection.Descending:
-                    _oComparers.Add((x, y) => _InvertResult(Comparer<P>.Default.Compare(poFunctor(x), poFunctor(y))));
-                    break;
-            }
+		public Sorter<T> ByComparison<P> (Func<T, P> poFunctor, SorterDirection peDirection)
+		{
+			switch (peDirection)
+			{
+			case SorterDirection.Ascending:
+				_oComparers.Add ((x, y) => Comparer<P>.Default.Compare (poFunctor (x), poFunctor (y)));
+				break;
+			case SorterDirection.Descending:
+				_oComparers.Add ((x, y) => _InvertResult (Comparer<P>.Default.Compare (poFunctor (x), poFunctor (y))));
+				break;
+			}
 
-            return this;
-        }
+			return this;
+		}
 
-        public Sorter<T> ByComparison(Func<T, T, Int32> poFunctor, SorterDirection peDirection)
-        {
-            switch (peDirection)
-            {
-                case SorterDirection.Ascending:
-                    _oComparers.Add((x, y) => poFunctor(x, y));
-                    break;
-                case SorterDirection.Descending:
-                    _oComparers.Add((x, y) => _InvertResult(poFunctor(x,y)));
-                    break;
-            }
+		public Sorter<T> ByComparison (Func<T, T, Int32> poFunctor, SorterDirection peDirection)
+		{
+			switch (peDirection)
+			{
+			case SorterDirection.Ascending:
+				_oComparers.Add ((x, y) => poFunctor (x, y));
+				break;
+			case SorterDirection.Descending:
+				_oComparers.Add ((x, y) => _InvertResult (poFunctor (x, y)));
+				break;
+			}
 
-            return this;
-        }
+			return this;
+		}
+		#region IComparer<T> Members
+		Int32 IComparer<T>.Compare (T x, T y)
+		{
+			foreach (var loComparer in _oComparers)
+			{
+				var lnResult = loComparer (x, y);
 
-        #region IComparer<T> Members
+				if (lnResult != 0)
+					return lnResult;
+			}
 
-        Int32 IComparer<T>.Compare(T x, T y)
-        {
-            foreach (var loComparer in _oComparers)
-            {
-                var lnResult = loComparer(x, y);
-
-                if (lnResult != 0)
-                    return lnResult;
-            }
-
-            return 0;
-        }
-
-        #endregion
-    }
+			return 0;
+		}
+		#endregion
+	}
 }
